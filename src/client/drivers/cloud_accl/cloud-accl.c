d720bfea2a7f163b93274547c5fe7b57bdc3e2fc
Arpit Gupta <glex.qsd@gmail.com>
2013-03-19 15:00:48 -0700

    Added logic for getting accelerometer data from cloud bridge. pending are designing the final evdev reporters

--- src/client/drivers/cloud_accl/cloud-accl.c
+++ src/client/drivers/cloud_accl/cloud-accl.c
@@ -32,36 +32,16 @@ struct input_dev *cloudaccl_input_dev;        /* Representation of an input devi
 static struct platform_device *cloudaccl_dev; /* Device structure */
 static int enable_update;
 
-// Thread Function for write_enable function
-/*int thread_fn() {
-
-    printk(KERN_INFO "thread1, enable_update:%d\n", enable_update);
-    if(enable_update == 1){
-        int x = 10000;
-        while(x>0){
-            if(x==9999)
-                printk(KERN_INFO "thread1, x:%d\n",x);
-            udelay(1000);
-            x-=1;
-            if(enable_update == 0){
-                printk(KERN_INFO "thread1 break, x:%d\n",x);
-                break;
-            }
-        }
-        
-    }
-    
-
-    return 0;
-}*/
-
 int thread_fn() {
     if(enable_update == 1){
         // ksocket things
             
         struct sockaddr_in addr_srv;
-        char buf[1024], *tmp;
-        int addr_len,len;
+        int char_size = 21;
+        char buf[char_size], *tmp;
+        int addr_len,len,ax,ay,az,count,axs,ays,azs;
+        //float adx, ady, adz;
+        
 
         #ifdef KSOCKET_ADDR_SAFE
         mm_segment_t old_fs;
@@ -94,7 +74,7 @@ int thread_fn() {
         kfree(tmp);
         len = sprintf(buf, "%s", "S\n");
         ksend(ca->sockfd_globl, buf, len, 0);
-        krecv(ca->sockfd_globl, buf, 1024, 0);
+        krecv(ca->sockfd_globl, buf, char_size, 0);
         printk("got message : %s", buf);
 
         
@@ -105,14 +85,24 @@ int thread_fn() {
             
             if(enable_update == 0){
                 printk(KERN_INFO "thread1 break\n");
-                printk(KERN_ALERT "Closing dell led socket now!\n\n");
+                printk(KERN_ALERT "Closing accl socket now!\n\n");
                 //kclose(sockfd_cli);
                 kclose(ca->sockfd_globl);
                 #ifdef KSOCKET_ADDR_SAFE
                     set_fs(old_fs);
                 #endif    
                 break;
-            }            
+            } 
+            char axc[2], ayc[2], azc[2], extra[10];
+            krecv(ca->sockfd_globl, buf, char_size, 0);
+            count = sscanf(buf, "%d,%d,%d,%d,%d,%d\n%s",&ax,&axs,&ay,&ays,&az,&azs,extra);
+            //printk("got while message : %s\n",buf);
+            //printk("ax:%f, ay:%f, az:%f, extra:%s\n", adx,ady,adz,extra); 
+            //printk("ax:%d, ay:%d, az:%d, extra:%s\n", ax,ay,az,extra);
+            printk("ax:%d, ay:%d, az:%d\n", ax,ay,az);     
+            printk("axs:%d, ays:%d, azs:%d\n", axs,ays,azs);          
+            //input_report_abs(cloudaccl_input_dev, ABS_X, 0); 
+            //input_report_abs(cloudaccl_input_dev, ABS_Y, 0); 
             schedule();
         }
         
@@ -177,79 +167,7 @@ static ssize_t write_enable(struct device *dev,
     else{
         printk ("case4: write_enable, only binary values\n");
         return count;
-    }
-
-    //printk ("write_enable callled with en:%d, enable_update:%d\n", en, enable_update);
-    /* Report relative ax via the
-       event interface */
-       
-    
-    
-    
-    
-    
-    
-    /*input_report_rel(cloudaccl_input_dev, REL_X, x);
-    input_report_rel(cloudaccl_input_dev, REL_Y, y);
-    input_sync(cloudaccl_input_dev);
-    
-    printk(KERN_ALERT "Creating Socket now\n");
-    //ksocket_t sockfd_cli;
-    //ca->sockfd_globl = sockfd_cli;
-    struct sockaddr_in addr_srv;
-    char buf[1024], *tmp;
-    int addr_len,len;
-
-#ifdef KSOCKET_ADDR_SAFE
-	mm_segment_t old_fs;
-	old_fs = get_fs();
-	set_fs(KERNEL_DS);
-#endif
-
-	sprintf(current->comm, "ksktcli");	
-	*buf = "Hello!";
-	memset(&addr_srv, 0, sizeof(addr_srv));
-	addr_srv.sin_family = AF_INET;
-	addr_srv.sin_port = htons(port);
-	addr_srv.sin_addr.s_addr = inet_addr("127.0.0.1");;
-	addr_len = sizeof(struct sockaddr_in);
-	
-	//sockfd_cli = ksocket(AF_INET, SOCK_STREAM, 0);
-	ca->sockfd_globl = ksocket(AF_INET, SOCK_STREAM, 0);
-	printk("sockfd_cli = 0x%p\n", ca->sockfd_globl);
-	if (ca->sockfd_globl == NULL)
-	{
-		printk("socket failed\n");
-		return -1;
-	}
-	if (kconnect(ca->sockfd_globl, (struct sockaddr*)&addr_srv, addr_len) < 0)
-	{
-		printk("connect failed\n");
-		return -1;
-	}
-
-	tmp = inet_ntoa(addr_srv.sin_addr);
-	printk("connected to : %s %d\n", tmp, ntohs(addr_srv.sin_port));
-	kfree(tmp);
-	
-	len = sprintf(buf, "%s", "S\n");
-    ksend(ca->sockfd_globl, buf, len, 0);
-	
-	krecv(ca->sockfd_globl, buf, 1024, 0);
-	printk("got message : %s", buf);
-
-	
-	
-	printk(KERN_ALERT "Closing dell led socket now!\n\n");
-	//kclose(sockfd_cli);
-	kclose(ca->sockfd_globl);
-#ifdef KSOCKET_ADDR_SAFE
-	set_fs(old_fs);
-#endif
-    
-    
-    
-    return count;*/
+    }    
 }
 
 /* Attach the sysfs write method */
@@ -267,9 +185,7 @@ static struct attribute_group cloudaccl_attr_group = {
 };
 
 /* Driver Initialization */
-int __init
-cloudaccl_init(void)
-{
+int __init cloudaccl_init(void){
     
     int err;
     ca = kzalloc(sizeof(*ca), GFP_KERNEL);
@@ -303,7 +219,6 @@ cloudaccl_init(void)
     set_bit(EV_REL, cloudaccl_input_dev->evbit);
     set_bit(REL_X, cloudaccl_input_dev->relbit);
     set_bit(REL_Y, cloudaccl_input_dev->relbit);
-
     /* Register with the input subsystem */
     input_register_device(cloudaccl_input_dev);
 
@@ -315,9 +230,7 @@ err_out:
 }
 
 /* Driver Exit */
-void
-cloudaccl_cleanup(void)
-{
+void cloudaccl_cleanup(void){
     /* Unregister from the input subsystem */
     input_unregister_device(cloudaccl_input_dev);
 
