35a5df8f185064c0bea6a4ed2474b3f95d7592d3
Arpit Gupta <glex.qsd@gmail.com>
2013-03-18 18:54:35 -0700

    Added the logic for enable/disable for accelerometer

--- src/client/drivers/cloud_accl/cloud-accl.c
+++ src/client/drivers/cloud_accl/cloud-accl.c
@@ -7,6 +7,9 @@
 #include <linux/input.h>
 #include <linux/platform_device.h>
 #include <linux/module.h>
+#include <linux/kthread.h>  // for threads
+#include <linux/sched.h>  // for task_struct
+#include <linux/delay.h>
 
 //Socket Header Files
 #include <linux/socket.h>
@@ -16,44 +19,183 @@
 
 static int port = 9988;
 
+struct cldaccl{
+    ksocket_t sockfd_globl;
+    struct mutex mutex; 
+
+};
+static struct cldaccl *ca;
+static struct task_struct *thread1;
+
 
 struct input_dev *cloudaccl_input_dev;        /* Representation of an input device */
 static struct platform_device *cloudaccl_dev; /* Device structure */
 static int enable_update;
 
+// Thread Function for write_enable function
+/*int thread_fn() {
+
+    printk(KERN_INFO "thread1, enable_update:%d\n", enable_update);
+    if(enable_update == 1){
+        int x = 10000;
+        while(x>0){
+            if(x==9999)
+                printk(KERN_INFO "thread1, x:%d\n",x);
+            udelay(1000);
+            x-=1;
+            if(enable_update == 0){
+                printk(KERN_INFO "thread1 break, x:%d\n",x);
+                break;
+            }
+        }
+        
+    }
+    
+
+    return 0;
+}*/
+
+int thread_fn() {
+    if(enable_update == 1){
+        // ksocket things
+            
+        struct sockaddr_in addr_srv;
+        char buf[1024], *tmp;
+        int addr_len,len;
+
+        #ifdef KSOCKET_ADDR_SAFE
+        mm_segment_t old_fs;
+        old_fs = get_fs();
+        set_fs(KERNEL_DS);
+        #endif
+
+        sprintf(current->comm, "ksktcli");	
+        /**buf = "Hello!";*/
+        memset(&addr_srv, 0, sizeof(addr_srv));
+        addr_srv.sin_family = AF_INET;
+        addr_srv.sin_port = htons(port);
+        addr_srv.sin_addr.s_addr = inet_addr("127.0.0.1");;
+        addr_len = sizeof(struct sockaddr_in);
+        
+        // Connect the socket
+        ca->sockfd_globl = ksocket(AF_INET, SOCK_STREAM, 0);
+        printk("sockfd_cli = 0x%p\n", ca->sockfd_globl);
+        if (ca->sockfd_globl == NULL){
+            printk("socket failed\n");
+            return -1;
+        }
+        if (kconnect(ca->sockfd_globl, (struct sockaddr*)&addr_srv, addr_len) < 0){
+            printk("connect failed\n");
+            return -1;
+        }       
+
+        tmp = inet_ntoa(addr_srv.sin_addr);
+        printk("connected to : %s %d\n", tmp, ntohs(addr_srv.sin_port));
+        kfree(tmp);
+        len = sprintf(buf, "%s", "S\n");
+        ksend(ca->sockfd_globl, buf, len, 0);
+        krecv(ca->sockfd_globl, buf, 1024, 0);
+        printk("got message : %s", buf);
+
+        
+        
+        
+        //sockfd_cli = ksocket(AF_INET, SOCK_STREAM, 0);
+        while(1){
+            
+            if(enable_update == 0){
+                printk(KERN_INFO "thread1 break\n");
+                printk(KERN_ALERT "Closing dell led socket now!\n\n");
+                //kclose(sockfd_cli);
+                kclose(ca->sockfd_globl);
+                #ifdef KSOCKET_ADDR_SAFE
+                    set_fs(old_fs);
+                #endif    
+                break;
+            }            
+            schedule();
+        }
+        
+    }
+    
+
+    return 0;
+}
+
 /* Sysfs method */
 static ssize_t read_enable(struct device *dev,
           struct device_attribute *attr,
           char *buffer)
-{
-   
+{   
     printk ("read_enable callled\n");
     return sprintf(buffer, "%u\n",enable_update);
 }
-
-
 
 static ssize_t write_enable(struct device *dev,
           struct device_attribute *attr,
           const char *buffer, size_t count)
 {
-    int en,x,y;
+    int en,x,y, enable_prev;
     x=0;
     y=1;
+    enable_prev = enable_update;
     
     sscanf(buffer, "%d", &en);
-    printk ("write_enable callled with prior enable_update:%d\n", enable_update);
+    //printk ("write_enable, prior enable_update:%d\n", enable_update);
+    
+    // Synchronize access to "enable" variable
+    mutex_lock(&ca->mutex);
     enable_update = en;
+    mutex_unlock(&ca->mutex);
+    if(enable_prev == 0 && enable_update ==0){
+        // Nothing to be done
+        printk ("case0:\n");
+        return count;
+    }
+    else if(enable_prev == 0 && enable_update ==1){
+        // It was not working, but we need to start the update process now
+        printk ("case1:\n");
+        char our_thread[8]="thread1";
+        thread1 = kthread_create(thread_fn,NULL,our_thread);
+        if((thread1))
+        {
+            //printk(KERN_INFO "in if\n");
+            wake_up_process(thread1);
+        }
+        return count;    
+    }
+    else if(enable_prev == 1 && enable_update ==0){
+        // It was updating, its time to turn it off, nothing to be done from this code section though
+        printk ("case2:\n");
+        return count;    
+    }
+    else if(enable_prev == 1 && enable_update ==1){
+        // It was updating, keep it running, nothing to be done from this code section though
+        printk ("case3:\n");
+        return count;    
+    }
+    else{
+        printk ("case4: write_enable, only binary values\n");
+        return count;
+    }
 
-    printk ("write_enable callled with en:%d, enable_update:%d\n", en, enable_update);
+    //printk ("write_enable callled with en:%d, enable_update:%d\n", en, enable_update);
     /* Report relative ax via the
        event interface */
-    input_report_rel(cloudaccl_input_dev, REL_X, x);
+       
+    
+    
+    
+    
+    
+    
+    /*input_report_rel(cloudaccl_input_dev, REL_X, x);
     input_report_rel(cloudaccl_input_dev, REL_Y, y);
     input_sync(cloudaccl_input_dev);
     
     printk(KERN_ALERT "Creating Socket now\n");
-    ksocket_t sockfd_cli;
+    //ksocket_t sockfd_cli;
+    //ca->sockfd_globl = sockfd_cli;
     struct sockaddr_in addr_srv;
     char buf[1024], *tmp;
     int addr_len,len;
@@ -64,22 +206,23 @@ static ssize_t write_enable(struct device *dev,
 	set_fs(KERNEL_DS);
 #endif
 
-	//sprintf(current->comm, "ksktcli");	
-	//*buf = "Hello!";
+	sprintf(current->comm, "ksktcli");	
+	*buf = "Hello!";
 	memset(&addr_srv, 0, sizeof(addr_srv));
 	addr_srv.sin_family = AF_INET;
 	addr_srv.sin_port = htons(port);
 	addr_srv.sin_addr.s_addr = inet_addr("127.0.0.1");;
 	addr_len = sizeof(struct sockaddr_in);
 	
-	sockfd_cli = ksocket(AF_INET, SOCK_STREAM, 0);
-	printk("sockfd_cli = 0x%p\n", sockfd_cli);
-	if (sockfd_cli == NULL)
+	//sockfd_cli = ksocket(AF_INET, SOCK_STREAM, 0);
+	ca->sockfd_globl = ksocket(AF_INET, SOCK_STREAM, 0);
+	printk("sockfd_cli = 0x%p\n", ca->sockfd_globl);
+	if (ca->sockfd_globl == NULL)
 	{
 		printk("socket failed\n");
 		return -1;
 	}
-	if (kconnect(sockfd_cli, (struct sockaddr*)&addr_srv, addr_len) < 0)
+	if (kconnect(ca->sockfd_globl, (struct sockaddr*)&addr_srv, addr_len) < 0)
 	{
 		printk("connect failed\n");
 		return -1;
@@ -90,22 +233,23 @@ static ssize_t write_enable(struct device *dev,
 	kfree(tmp);
 	
 	len = sprintf(buf, "%s", "S\n");
-    ksend(sockfd_cli, buf, len, 0);
+    ksend(ca->sockfd_globl, buf, len, 0);
 	
-	krecv(sockfd_cli, buf, 1024, 0);
+	krecv(ca->sockfd_globl, buf, 1024, 0);
 	printk("got message : %s", buf);
 
 	
 	
 	printk(KERN_ALERT "Closing dell led socket now!\n\n");
-	kclose(sockfd_cli);
+	//kclose(sockfd_cli);
+	kclose(ca->sockfd_globl);
 #ifdef KSOCKET_ADDR_SAFE
 	set_fs(old_fs);
 #endif
     
     
     
-    return count;
+    return count;*/
 }
 
 /* Attach the sysfs write method */
@@ -126,6 +270,18 @@ static struct attribute_group cloudaccl_attr_group = {
 int __init
 cloudaccl_init(void)
 {
+    
+    int err;
+    ca = kzalloc(sizeof(*ca), GFP_KERNEL);
+    
+    if(!ca){
+        err = -ENOMEM;
+        printk ("cloudaccl_init: error mem alloc\n");
+        goto err_out;
+    }
+    
+    mutex_init(&ca->mutex);
+    
     /* Register a platform device */
     cloudaccl_dev = platform_device_register_simple("cloudaccl", -1, NULL, 0);
     if (IS_ERR(cloudaccl_dev)){
@@ -153,6 +309,9 @@ cloudaccl_init(void)
 
     printk("Cloud Accelerometer Driver Initialized.\n");
     return 0;
+    
+err_out:
+    return ERR_PTR(err);
 }
 
 /* Driver Exit */
